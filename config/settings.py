55e74d221ef0de150d022c18027ef34ce4c8c4b3
Gerard Safont <gerymaps200515@gmail.com>
2026-06-15 22:39:11 +0200

    feat: finish the urls config

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1',]
 
 
 # Application definition
