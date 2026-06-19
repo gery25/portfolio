12349e021d2f6b45a1c2bf833cfb90eef82cdc47
Gerard Safont <gerymaps200515@gmail.com>
2026-06-19 10:53:36 +0200

    feat: add projects and skill models in dev portfolio

--- developer_portfolio/models.py
+++ developer_portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='developer_portfolio/static/developer_portfolio/image/')
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=200)
+    github_link = models.URLField(blank=True, null=True)
+
+class Skill(models.Model):
+    class Category(models.TextChoices):
+        FRONTEND = 'Frontend', 'Frontend'
+        BACKEND = 'Backend', 'Backend'
+        DATABASE = 'Database', 'Database'
+        DEVOPS = 'DevOps', 'DevOps'
+        OTHER = 'Other', 'Other'
+    
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=Category.choices)
+    icon = models.ImageField(upload_to='developer_portfolio/static/developer_portfolio/image/icons/')
