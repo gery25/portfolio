6a66e37fcd6c7ae3925e31ee8bf9a977319ffc88
Gerard Safont <gerymaps200515@gmail.com>
2026-06-19 11:25:55 +0200

    feat: modify the views of home page

--- developer_portfolio/views.py
+++ developer_portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'developer_portfolio/base.html')
+    top_projects = Project.objects.all()[:3]
+    return render(request, 'developer_portfolio/home.html', {'top_projects': top_projects})
