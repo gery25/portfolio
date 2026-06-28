4cbfbdda5d67e5791f5f9a973c96734ad05a51d5
gerard <gerymaps200515@gmail.com>
2026-06-28 14:07:32 +0200

    feat: add the app in setings and admin file

--- developer_portfolio/admin.py
+++ developer_portfolio/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from .models import Project, Skill
 
 # Register your models here.
 
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(TranslationAdmin):
+    pass
 admin.site.register(Skill)
